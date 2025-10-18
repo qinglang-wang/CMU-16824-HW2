61c58e7c1453d2fe385a3a9823669387f251a10f
qinglang-wang <qinglang.wang.cmu@gmail.com>
2025-10-18 12:58:12 -0400

    [Feat] Enable training logistics and prefecter on win

--- gan/train.py
+++ gan/train.py
@@ -8,19 +8,20 @@ from torchvision.utils import save_image
 from PIL import Image
 from torchvision.datasets import VisionDataset
 
+def rescaling(x):
+    return (x - 0.5) * 2.0
 
 def build_transforms():
     # 1. Convert input image to tensor.
     # 2. Rescale input image from [0., 1.] to be between [-1., 1.].
-    rescaling = lambda x: (x - 0.5) * 2.0
     ds_transforms = transforms.Compose([transforms.ToTensor(), rescaling])
     return ds_transforms
 
 
 def get_optimizers_and_schedulers(gen, disc):
     # Get optimizers and learning rate schedulers.
-    optim_discriminator = torch.optim.Adam(disc.parameters(), lr=2e-4, betas=(0, 0.9))
-    optim_generator = torch.optim.Adam(gen.parameters(), lr=2e-4, betas=(0, 0.9))
+    optim_discriminator = torch.optim.Adam(disc.parameters(), lr=2e-4, betas=(0., 0.9))
+    optim_generator = torch.optim.Adam(gen.parameters(), lr=2e-4, betas=(0., 0.9))
     ##################################################################
     # TODO 1.2: Construct the learning rate schedulers for the
     # generator and discriminator. The learning rate for the
@@ -63,6 +64,34 @@ class Dataset(VisionDataset):
         return len(self.file_names)
 
 
+class Prefetcher:
+    def __init__(self, loader, device):
+        self.data_loader = loader 
+        self.device = device
+
+        self.iterator = iter(loader) 
+        self.stream = torch.cuda.Stream(device=device)
+        
+        self.preload()
+
+    def preload(self):
+        try:
+            self.next_batch = next(self.iterator)
+        except StopIteration:
+            self.iterator = iter(self.data_loader)
+            self.next_batch = next(self.iterator)
+            
+        with torch.cuda.stream(self.stream):
+            self.next_batch = self.next_batch.to(self.device, non_blocking=True)
+
+    def next(self):
+        torch.cuda.current_stream(device=self.device).wait_stream(self.stream)
+        batch = self.next_batch
+        
+        self.preload()
+        return batch
+
+
 def train_model(
     gen,
     disc,
@@ -81,8 +110,10 @@ def train_model(
         Dataset(root="../datasets/CUB_200_2011_32", transform=ds_transforms),
         batch_size=batch_size,
         shuffle=True,
-        num_workers=4,
+        num_workers=8,
         pin_memory=True,
+        persistent_workers=True,
+        prefetch_factor=2
     )
 
     (
@@ -98,111 +129,117 @@ def train_model(
     fids_list = []
     iters_list = []
     pbar = tqdm(total = num_iterations)
-    while iters < num_iterations:
-        for train_batch in train_loader:
-            with torch.amp.autocast('cuda', enabled=amp_enabled):
-                train_batch = train_batch.cuda()
-                
-                ####################### UPDATE DISCRIMINATOR #####################
-                ##################################################################
-                # TODO 1.2: compute generator, discriminator, and interpolated outputs
-                # 1. Compute generator output
-                # Note: The number of samples must match the batch size.
-                # 2. Compute discriminator output on the train batch.
-                # 3. Compute the discriminator output on the generated data.
-                ##################################################################
-                discrim_real = disc.forward(train_batch)
-                discrim_fake = disc.forward(gen.forward(train_batch.shape[0]))
-                ##################################################################
-                #                          END OF YOUR CODE                      #
-                ##################################################################
+    prefetcher = Prefetcher(train_loader, 'cuda')
+    while iters <= num_iterations:
+        train_batch = prefetcher.next()
+        with torch.amp.autocast('cuda', enabled=amp_enabled):
+            ####################### UPDATE DISCRIMINATOR #####################
+            ##################################################################
+            # TODO 1.2: compute generator, discriminator, and interpolated outputs
+            # 1. Compute generator output
+            # Note: The number of samples must match the batch size.
+            # 2. Compute discriminator output on the train batch.
+            # 3. Compute the discriminator output on the generated data.
+            ##################################################################
+            generated_batch = gen.forward(train_batch.shape[0]).detach()
+            discrim_real = disc.forward(train_batch)
+            discrim_fake = disc.forward(generated_batch)
+            ##################################################################
+            #                          END OF YOUR CODE                      #
+            ##################################################################
 
-                ##################################################################
-                # TODO 1.5 Compute the interpolated batch and run the
-                # discriminator on it.
-                ###################################################################
-                interp = None
-                discrim_interp = None
-                ##################################################################
-                #                          END OF YOUR CODE                      #
-                ##################################################################
-
-            discriminator_loss = disc_loss_fn(
-                discrim_real, discrim_fake, discrim_interp, interp, lamb
-            )
+            ##################################################################
+            # TODO 1.5 Compute the interpolated batch and run the
+            # discriminator on it.
+            ###################################################################
+            epsilon = torch.rand(train_batch.shape[0], 1, 1, 1, device='cuda')
             
-            optim_discriminator.zero_grad(set_to_none=True)
-            scaler.scale(discriminator_loss).backward()
-            scaler.step(optim_discriminator)
-            scheduler_discriminator.step()
+            interp = epsilon * train_batch + (1 - epsilon) * generated_batch
+            interp.requires_grad_(True)
 
-            if iters % 5 == 0:
+            with torch.amp.autocast('cuda', enabled=amp_enabled):
+                discrim_interp = disc.forward(interp)
+            # discrim_interp, interp = None, None
+            ##################################################################
+            #                          END OF YOUR CODE                      #
+            ##################################################################
+
+        discriminator_loss = disc_loss_fn(
+            discrim_real, discrim_fake, discrim_interp, interp, lamb
+        )
+        
+        optim_discriminator.zero_grad(set_to_none=True)
+        scaler.scale(discriminator_loss).backward()
+        scaler.step(optim_discriminator)
+        scheduler_discriminator.step()
+
+        if iters % 5 == 0:
+            with torch.amp.autocast('cuda', enabled=amp_enabled):
+                ##################################################################
+                # TODO 1.2: Compute generator and discriminator output on
+                # generated data.
+                ###################################################################
+                fake_batch = gen.forward(batch_size)
+                discrim_fake = disc.forward(fake_batch)
+                ##################################################################
+                #                          END OF YOUR CODE                      #
+                ##################################################################
+
+                generator_loss = gen_loss_fn(discrim_fake)
+
+            optim_generator.zero_grad(set_to_none=True)
+            scaler.scale(generator_loss).backward()
+            scaler.step(optim_generator)
+            scheduler_generator.step()
+
+        if iters % log_period == 0 and iters != 0:
+            with torch.no_grad():
                 with torch.amp.autocast('cuda', enabled=amp_enabled):
                     ##################################################################
-                    # TODO 1.2: Compute generator and discriminator output on
-                    # generated data.
-                    ###################################################################
-                    fake_batch = gen.forward(batch_size)
-                    discrim_fake = disc.forward(fake_batch)
+                    # TODO 1.2: Generate samples using the generator.
+                    # Make sure they lie in the range [0, 1]!
+                    ##################################################################
+                    generated_samples = (gen.forward(100) + 1) / 2
                     ##################################################################
                     #                          END OF YOUR CODE                      #
                     ##################################################################
+                save_image(
+                    generated_samples.data.float(),
+                    prefix + "samples_{}.png".format(iters),
+                    nrow=10,
+                )
+                if os.environ.get('PYTORCH_JIT', 1):
+                    torch.jit.save(torch.jit.script(gen), prefix + "/generator.pt")
+                    torch.jit.save(torch.jit.script(disc), prefix + "/discriminator.pt")
+                else:
+                    torch.save(gen, prefix + "/generator.pt")
+                    torch.save(disc, prefix + "/discriminator.pt")
+                fid = get_fid(
+                    gen,
+                    dataset_name="cub",
+                    dataset_resolution=32,
+                    z_dimension=128,
+                    batch_size=256,
+                    num_gen=10_000,
+                )
+                print(f"Iteration {iters} FID: {fid}")
+                fids_list.append(fid)
+                iters_list.append(iters)
 
-                    generator_loss = gen_loss_fn(discrim_fake)
-
-                optim_generator.zero_grad(set_to_none=True)
-                scaler.scale(generator_loss).backward()
-                scaler.step(optim_generator)
-                scheduler_generator.step()
-
-            if iters % log_period == 0 and iters != 0:
-                with torch.no_grad():
-                    with torch.amp.autocast('cuda', enabled=amp_enabled):
-                        ##################################################################
-                        # TODO 1.2: Generate samples using the generator.
-                        # Make sure they lie in the range [0, 1]!
-                        ##################################################################
-                        generated_samples = (gen.forward() + 1) / 2
-                        ##################################################################
-                        #                          END OF YOUR CODE                      #
-                        ##################################################################
-                    save_image(
-                        generated_samples.data.float(),
-                        prefix + "samples_{}.png".format(iters),
-                        nrow=10,
-                    )
-                    if os.environ.get('PYTORCH_JIT', 1):
-                        torch.jit.save(torch.jit.script(gen), prefix + "/generator.pt")
-                        torch.jit.save(torch.jit.script(disc), prefix + "/discriminator.pt")
-                    else:
-                        torch.save(gen, prefix + "/generator.pt")
-                        torch.save(disc, prefix + "/discriminator.pt")
-                    fid = get_fid(
-                        gen,
-                        dataset_name="cub",
-                        dataset_resolution=32,
-                        z_dimension=128,
-                        batch_size=256,
-                        num_gen=10_000,
-                    )
-                    print(f"Iteration {iters} FID: {fid}")
-                    fids_list.append(fid)
-                    iters_list.append(iters)
-
-                    save_plot(
-                        iters_list,
-                        fids_list,
-                        xlabel="Iterations",
-                        ylabel="FID",
-                        title="FID vs Iterations",
-                        filename=prefix + "fid_vs_iterations",
-                    )
-                    interpolate_latent_space(
-                        gen, prefix + "interpolations_{}.png".format(iters)
-                    )
-            scaler.update()
-            iters += 1
-            pbar.update(1)
+                save_plot(
+                    iters_list,
+                    fids_list,
+                    xlabel="Iterations",
+                    ylabel="FID",
+                    title="FID vs Iterations",
+                    filename=prefix + "fid_vs_iterations",
+                )
+                interpolate_latent_space(
+                    gen, prefix + "interpolations_{}.png".format(iters)
+                )
+        scaler.update()
+        iters += 1
+        pbar.update(1)
     fid = get_fid(
         gen,
         dataset_name="cub",
